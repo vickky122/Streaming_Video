8d6eae84a573b6541705536e2399968bc60c4057
vickky122 <vikrantmani45@gmail.com>
2024-08-14 19:23:37 +0530

    first testing of the post method - video,filename and description(post)

---
+++ spring-stream-backend/src/main/java/com/stream/app/controllers/VideoController.java
@@ -0,0 +1,43 @@
+package com.stream.app.controllers;
+
+import com.stream.app.entities.Video;
+import com.stream.app.playload.CustomMessage;
+import com.stream.app.services.VideoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/videos")
+@CrossOrigin("*")
+public class VideoController {
+
+    private VideoService videoService;
+
+    public VideoController(VideoService videoService) {
+        this.videoService = videoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("title") String title,
+            @RequestParam("description") String description
+    ) {
+
+        Video video = new Video();
+        video.setTitle(title);
+        video.setDescription(description);
+        video.setVideoId(UUID.randomUUID().toString());
+
+        videoService.save(video, file);
+
+        return null;
+        }
+
+
+    }
+
--- spring-stream-backend/src/main/java/com/stream/app/services/impl/VideoServiceImpl.java
+++ spring-stream-backend/src/main/java/com/stream/app/services/impl/VideoServiceImpl.java
@@ -2,30 +2,58 @@ package com.stream.app.services.impl;
 
 import com.stream.app.entities.Video;
 import com.stream.app.services.VideoService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Service
 public class VideoServiceImpl implements VideoService {
+
+    @Value("${files.video}")
+    String DIR;
+
     @Override
     public Video save(Video video, MultipartFile file) {
 
-        //Original files name : image.png-> image
-        String filename=file.getOriginalFilename();
+        try {
 
 
-        // create folder path
-        //
+            //Original files name : image.png-> image
 
-        // folder pathname with filename
 
-        //copy file to the folder
+            String filename = file.getOriginalFilename();
+            String contentType = file.getContentType();
+            InputStream inputStream = file.getInputStream();
 
-        //make video metadata
 
-        // save metadata
+
+
+            // create folder path
+            String cleanFileName=StringUtils.cleanPath(filename);
+            String cleanFolder=StringUtils.cleanPath(DIR);
+
+            Path path=Paths.get(cleanFolder,cleanFileName);
+            System.out.println(path);
+
+
+            // folder pathname with filename
+
+            //copy file to the folder
+
+            //make video metadata
+
+            // save metadata
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
 
         return null;
     }
